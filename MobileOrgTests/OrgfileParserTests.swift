d783348aa04b871e002beb415d8b84965829893e
Mario Martelli <cocoa@schnuddelhuddel.de>
2016-12-20 09:44:03 +0100

    Test reproducing the error

--- MobileOrgTests/OrgfileParserTests.swift
+++ MobileOrgTests/OrgfileParserTests.swift
@@ -11,14 +11,46 @@ import CoreData
 
 class OrgfileParserTests: XCTestCase {
   var moc:NSManagedObjectContext?
-  var url:URL!
+
+
+  // Tackles bug described in:
+  // https://github.com/MobileOrg/mobileorg/issues/86
+  func testParseOrgFileDefaultTodoWordsBug() {
+    let parser = OrgFileParser()
+//    var bundle = Bundle(for: type(of: self))
+//    var url = bundle.url(forResource: "checksums", withExtension: "dat")
+
+    let bundle = Bundle(for: type(of: self))
+    let url = bundle.url(forResource: "defaultTodoWords", withExtension: "org")
+    parser.localFilename = url?.relativePath
+
+    parser.orgFilename = "index.org"
+    parser.parse(moc)
+
+    try! moc?.save()
+
+    let fetchRequest = NSFetchRequest<Node>(entityName: "Node")
+    fetchRequest.predicate = NSPredicate (format: "outlinePath == %@", "olp:Heading:MobileOrg Missing Features/Localisation")
+
+    do {
+      let nodes = try moc!.fetch(fetchRequest)
+
+      XCTAssertEqual(nodes.count, 1)
+
+
+    } catch _ { XCTFail() }
+  }
+
 
 
   // Tackles bug described in:
   // https://github.com/MobileOrg/mobileorg/issues/62
   func testParseOrgFileSkippingHeadingBug() {
     let parser = OrgFileParser()
-    parser.localFilename = url.relativePath
+    let bundle = Bundle(for: type(of: self))
+    let url = bundle.url(forResource: "headingskip", withExtension: "org")
+
+    parser.localFilename = url?.relativePath
     parser.orgFilename = "Heading"
     parser.parse(moc)
 
@@ -56,8 +88,6 @@ class OrgfileParserTests: XCTestCase {
     let context = setUpInMemoryManagedObjectContext()
     moc = context
     // Put setup code here. This method is called before the invocation of each test method in the class.
-    let bundle = Bundle(for: type(of: self))
-    url = bundle.url(forResource: "headingskip", withExtension: "org")
   }
 
   override func tearDown() {
